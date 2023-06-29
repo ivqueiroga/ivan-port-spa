08666eb9f83798b465ce703e78d7e1a08c774aff
ivqueiroga <ivanqueirogap@gmail.com>
2023-06-29 15:08:37 -0300

    updt

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -3,7 +3,7 @@ import { IContact } from '../Interface/Interface';
 import './Contacts.css';
 import emailjs from '@emailjs/browser';
 import {DiGithubBadge} from 'react-icons/di';
-import {MdEmail} from 'react-icons/md';
+// import {MdEmail} from 'react-icons/md';
 import {FaLinkedin, FaWhatsapp}  from 'react-icons/fa';
 
 const ALERT = {
@@ -58,9 +58,10 @@ function Contacts() {
       <form ref={form} method="post" onSubmit={handleSubmit} className='form-container'>
         <h2>- Contato -</h2>
         <div className='input-container'>
-          <label className='input-areas'>
+          <label className='input-areas' htmlFor="from_name">
               Email:
               <textarea
+                id={"from_name"}
                 placeholder='Digite aqui seu Email'
                 name="from_name"
                 defaultValue={contact.from_name}
@@ -69,9 +70,10 @@ function Contacts() {
                 cols={40}
               />
           </label>
-          <label className='input-areas'>
+          <label className='input-areas' htmlFor="subject">
             Assunto:
             <textarea
+              id={"subject"}
               name="subject"
               defaultValue={contact.subject}
               onChange={handleChange}
@@ -79,9 +81,10 @@ function Contacts() {
               cols={40}
             />
           </label>
-          <label className='input-areas message'>
+          <label className='input-areas message' htmlFor="message">
             Mensagem:
             <textarea
+              id={"message"}
               name="message"
               defaultValue={contact.message}
               onChange={handleChange}
@@ -95,30 +98,32 @@ function Contacts() {
     <div className='social-container'>
       <h3>Redes Sociais</h3>
       <div className='social-link-container'>
-        <label>
-          <a href={links.github} target='_blank' rel='noreferrer'>
-            <DiGithubBadge className='social-icon' size={ICONSIZE}/>
-            /ivqueiroga
-          </a>
-        </label>
-        <label>
-          <a href={links.linkedin} target='_blank' rel='noreferrer'>
-          <FaLinkedin className='social-icon' size={ICONSIZE}/>
-            /in/ivanqueirogap/
-          </a>
-        </label>
-        <label>
-          <div>
-            <FaWhatsapp className='social-icon' size={ICONSIZE}/>
-            {links.phone}
+        <div className='social-link-org'>
+          <div className='social-text'>
+            <a href={links.github} target='_blank' rel='noreferrer'>
+              <DiGithubBadge className='social-icon' size={ICONSIZE}/>
+              /ivqueiroga
+            </a>
           </div>
-        </label>
-        <label>
-          <div>
-            <MdEmail className='social-icon' size={ICONSIZE}/>
-            {links.email}
+          <div className='social-text'>
+            <a href={links.linkedin} target='_blank' rel='noreferrer'>
+              <FaLinkedin className='social-icon' size={ICONSIZE}/>
+              /in/ivanqueirogap/
+            </a>
           </div>
-        </label>
+          <div className='social-text'>
+            <div>
+              <FaWhatsapp className='social-icon' size={ICONSIZE}/>
+              {links.phone}
+            </div>
+          </div>
+          <div className='social-text'>
+            <div>
+              {/* <MdEmail className='social-icon' size={ICONSIZE}/> */}
+              {links.email}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     </div>
